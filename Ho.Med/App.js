06eaff94bc5b1d522cbed0276702df9614ef87f7
FouedCharaabi <chfoued75@gmail.com>
2022-01-07 11:13:27 +0100

    LandingPage,SignIn,Signup Added

--- Ho.Med/App.js
+++ Ho.Med/App.js
@@ -1,20 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Navbar from './screens/Navbar';
-import Localisation from './screens/Localisation';
-import Cart from './screens/Cart';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Navbar from "./screens/Navbar";
+import Localisation from "./screens/Localisation";
+import Cart from "./screens/Cart";
+import RegisterScreen from "./screens/RegisterScreen";
+import Login from "./screens/SigninScreen";
+import LandingPage from "./screens/LandingPage";
 
-const Stack = createNativeStackNavigator()
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="navbar" component={Navbar} options={{headerShown: false}} />
-        <Stack.Screen name="localisation" component={Localisation} options={{headerShown: false}} />
-        <Stack.Screen name="cart" component={Cart} options={{headerShown: false}} />
+        <Stack.Screen
+          name="landingPage"
+          component={LandingPage}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="registerScreen"
+          component={RegisterScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="login"
+          component={Login}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="navbar"
+          component={Navbar}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="localisation"
+          component={Localisation}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="cart"
+          component={Cart}
+          options={{ headerShown: false }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ Ho.Med/screens/SigninScreen.js
@@ -0,0 +1,179 @@
+import React, { Component } from "react";
+import {
+  SafeAreaView,
+  StatusBar,
+  View,
+  Text,
+  BackHandler,
+  StyleSheet,
+  TextInput,
+  ScrollView,
+  Image,
+  TouchableOpacity,
+} from "react-native";
+import { withNavigation } from "react-navigation";
+import { Colors, Fonts, Sizes } from "../constant/styles";
+import { MaterialIcons } from "@expo/vector-icons";
+import { TransitionPresets } from "react-navigation-stack";
+
+class SigninScreen extends Component {
+  componentDidMount() {
+    BackHandler.addEventListener(
+      "hardwareBackPress",
+      this.handleBackButton.bind(this)
+    );
+  }
+
+  componentWillUnmount() {
+    BackHandler.removeEventListener(
+      "hardwareBackPress",
+      this.handleBackButton.bind(this)
+    );
+  }
+
+  handleBackButton = () => {
+    this.props.navigation.pop();
+    return true;
+  };
+
+  state = {
+    fullName: "",
+    password: "",
+    emailAddress: "",
+  };
+
+  render() {
+    return (
+      <SafeAreaView style={{ flex: 1, backgroundColor: Colors.bodyBackColor }}>
+        <StatusBar backgroundColor={Colors.primaryColor} />
+        <View style={{ flex: 1 }}>
+          {this.backArrow()}
+          <ScrollView showsVerticalScrollIndicator={false}>
+            {this.appLogo()}
+            {this.emailAddressTextField()}
+            {this.passwordTextField()}
+            {this.continueButton()}
+          </ScrollView>
+        </View>
+      </SafeAreaView>
+    );
+  }
+
+  backArrow() {
+    return (
+      <MaterialIcons
+        onPress={() => this.props.navigation.push("RegisterScreen")}
+        name="arrow-back"
+        size={24}
+        color="black"
+        style={{
+          marginHorizontal: Sizes.fixPadding * 2.0,
+          marginTop: Sizes.fixPadding * 2.0,
+        }}
+        onPress={() => this.props.navigation.pop()}
+      />
+    );
+  }
+
+  emailAddressTextField() {
+    return (
+      <TextInput
+        placeholder="Email Address"
+        placeholderTextColor={Colors.primaryColor}
+        value={this.state.emailAddress}
+        onChangeText={(text) => this.setState({ emailAddress: text })}
+        selectionColor={Colors.primaryColor}
+        style={styles.textFieldStyle}
+      />
+    );
+  }
+
+  passwordTextField() {
+    return (
+      <TextInput
+        placeholder="Password"
+        placeholderTextColor={Colors.primaryColor}
+        value={this.state.password}
+        onChangeText={(text) => this.setState({ password: text })}
+        secureTextEntry={true}
+        selectionColor={Colors.primaryColor}
+        style={styles.textFieldStyle}
+      />
+    );
+  }
+
+  registerText() {
+    return (
+      <Text
+        style={{
+          marginBottom: Sizes.fixPadding + 10.0,
+          ...Fonts.primaryColor18Medium,
+          textAlign: "center",
+        }}
+      >
+        Sign In
+      </Text>
+    );
+  }
+
+  continueButton() {
+    return (
+      <TouchableOpacity
+        onPress={() => this.props.navigation.push("navbar")}
+        activeOpacity={0.9}
+        style={styles.continueButtonStyle}
+      >
+        <Text style={{ ...Fonts.whiteColor19Medium }}>Sign In</Text>
+      </TouchableOpacity>
+    );
+  }
+
+  appLogo() {
+    return (
+      <Image
+        source={require("../assets/images/transparent-icon.png")}
+        style={styles.appLogoStyle}
+        resizeMode="contain"
+      />
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  continueButtonStyle: {
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: Colors.primaryColor,
+    paddingVertical: Sizes.fixPadding,
+    marginHorizontal: Sizes.fixPadding,
+    borderRadius: Sizes.fixPadding - 5.0,
+    marginTop: Sizes.fixPadding * 4.0,
+  },
+  appLogoStyle: {
+    width: 200.0,
+    height: 200.0,
+    alignSelf: "center",
+    marginBottom: Sizes.fixPadding,
+    marginTop: Sizes.fixPadding * 3.0,
+  },
+  textFieldStyle: {
+    borderColor: "rgba(0, 150, 136, 0.3)",
+    borderWidth: 1.0,
+    borderRadius: Sizes.fixPadding - 5.0,
+    paddingHorizontal: Sizes.fixPadding * 2.0,
+    height: 55.0,
+    ...Fonts.primaryColor18Medium,
+    marginHorizontal: Sizes.fixPadding,
+    backgroundColor: Colors.whiteColor,
+    marginTop: Sizes.fixPadding * 2.0,
+  },
+});
+
+SigninScreen.navigationOptions = () => {
+  return {
+    header: () => null,
+    ...TransitionPresets.SlideFromRightIOS,
+  };
+};
+
+export default withNavigation(SigninScreen);
